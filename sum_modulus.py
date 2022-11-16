51b99f6b85e0ec0236b417fa91ae9ef8d97bc432
Kent Gatera <kent.gatera@stu.ocsb.ca>
2022-11-16 19:59:15 +0000

    This program adds several numbers together using a modulus.

---
+++ sum_modulus.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Created By: Kent Gatera
+# Created: Nov 13, 2022
+# This program adds several numbers together using a modulus.
+
+
+def main():
+
+    try:
+        num_user = int(input("What is the range from 0?: "))
+        new_int = 0
+        loop_count = 0
+
+        # Main loop
+        while True:
+            old_int = 0
+            # This section retrieves all the number from 0-user wished number
+            # That is divisible by 3 or 5 and adds it
+            for i in range(0, num_user, 1):
+                if (i % 3 == 0) or (i % 5 == 0):
+                    new_int += i
+                    print(f"{i}+{old_int} = {new_int}")
+                    loop_count += 1
+                    old_int = new_int
+
+            # The program breaks and goes on forever if the break number is not odd.
+            if i == (num_user - 1):
+                break
+    except:
+        print(f"Your input is not a number. (e.g 50)")
+
+
+# Running the program
+if __name__ == "__main__":
+    main()
